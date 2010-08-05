28f85daf2f97d35387c9ebccbf54282bd5d39a93
keny.plb <keny.plb@72d34acd-96ab-fc25-247f-fc063804e312>
2010-08-05 09:09:09 +0000

    operate miss

--- branches/add-new-feature-&-fix-bug/plugin/build-mac.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-mkdir -p npcapture.plugin/Contents/MacOS
-cp -f Info.plist npcapture.plugin/Contents
-g++ -framework Cocoa -DMAC \
-  -DWEBKIT_DARWIN_SDK -Wno-write-strings -lresolv \
-  -o npcapture.plugin/Contents/MacOS/npcapture \
-  -arch i386 -bundle \
-  npcapture.cpp np_entry.cpp plugin.cpp save.cpp savepanel.mm
