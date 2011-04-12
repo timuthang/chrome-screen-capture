621fdc2c559b0836074982e183b093b9df5bdd49
jingzhao@google.com <jingzhao@google.com@72d34acd-96ab-fc25-247f-fc063804e312>
2011-04-12 03:27:31 +0000

    Remove tags/, and move code from trunk/ to trunk/src/. Put codereview.settings under trunk/ and branches/.

--- tags/release-2.0.4/plugin/build-mac.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-mkdir -p screen_capture.plugin/Contents/MacOS
-cp -f Info.plist screen_capture.plugin/Contents
-g++ -framework Cocoa -DMAC \
-  -DWEBKIT_DARWIN_SDK -Wno-write-strings -lresolv -arch i386 -bundle \
-  -isysroot /Developer/SDKs/MacOSX10.5.sdk -mmacosx-version-min=10.5 \
-  -o screen_capture.plugin/Contents/MacOS/screen_capture \
-   np_entry.cc npp_entry.cc plugin.cc screen_capture.cc screen_capture.mm
