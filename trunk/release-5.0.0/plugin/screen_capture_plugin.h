b66cc5e19b3137a779d9012d3e7daecc32b504ae
jingzhao@google.com <jingzhao@google.com@72d34acd-96ab-fc25-247f-fc063804e312>
2011-06-10 09:00:23 +0000

    Release 5.0.0

---
+++ trunk/release-5.0.0/plugin/screen_capture_plugin.h
@@ -0,0 +1,45 @@
+#ifndef SCREEN_CAPTURE_PLUGIN_H_
+#define SCREEN_CAPTURE_PLUGIN_H_
+
+#include "npapi.h"
+#include "npruntime.h"
+#include "npfunctions.h"
+#include "plugin_base.h"
+#include "script_object_base.h"
+
+#ifdef _WINDOWS
+#include <windows.h>
+#define WM_CAPTURESCREEN  WM_USER+1001
+#endif
+
+class ScreenCapturePlugin : public PluginBase {
+public:
+  ScreenCapturePlugin() {}
+  virtual ~ScreenCapturePlugin() {}
+    
+  NPError Init(NPP instance, uint16_t mode, int16_t argc, char* argn[],
+               char* argv[], NPSavedData* saved);
+  NPError UnInit(NPSavedData** saved);
+  NPError GetValue(NPPVariable variable, void *value);
+  NPError SetWindow(NPWindow* window);
+
+  static PluginBase* CreateObject() { return new ScreenCapturePlugin; }
+
+  void CaptureScreen();
+  void CaptureScreenCallback(BYTE* image_data, int image_data_len);
+
+#ifdef _WINDOWS
+  void SetButtonMessage(WCHAR* ok_caption, WCHAR* cancel_caption);
+  bool SetHotKey(int keycode);
+  static LRESULT WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+#endif
+
+private:
+  ScriptObjectBase* script_object_;
+#ifdef _WINDOWS
+  WNDPROC old_proc_;
+  static int keycode_;
+#endif
+};
+
+#endif // SCREEN_CAPTURE_PLUGIN_H_
