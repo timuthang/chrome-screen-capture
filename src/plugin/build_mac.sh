991e1381457d8c9ccd9e9c5b821b1241daf35f81
jingzhao@google.com <jingzhao@google.com@72d34acd-96ab-fc25-247f-fc063804e312>
2013-03-27 14:20:14 +0000

    Update binaries.

--- src/plugin/build_mac.sh
+++ src/plugin/build_mac.sh
@@ -3,7 +3,8 @@ mkdir -p screen_capture.plugin/Contents/MacOS
 cp -f Info.plist screen_capture.plugin/Contents
 g++ -framework Cocoa -DMAC \
   -DWEBKIT_DARWIN_SDK -Wno-write-strings -lresolv -arch i386 -bundle \
-  -isysroot /Developer/SDKs/MacOSX10.5.sdk -mmacosx-version-min=10.5 \
+  -isysroot /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.8.sdk/ \
+  -mmacosx-version-min=10.5 \
   -o screen_capture.plugin/Contents/MacOS/screen_capture \
   log.cc np_entry.cc npn_entry.cc npp_entry.cc plugin_base.cc \
   plugin_factory.cc screen_capture.mm screen_capture_plugin.cc \
